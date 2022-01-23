7ec5bc6cc21621bbf40fafc8a9a1527b7cc2d7b7
Carlo Sacchi Accenture <carlo.sacchi@accenture.com>
2022-01-23 20:14:56 +0100

    Added some fix and Change type

--- Start_Stop_v2.cmd
+++ Start_Stop_v2.cmd
@@ -6,7 +6,7 @@ aws iam list-account-aliases --output table --color off
 
 echo -----------------------------------------
 
-aws ec2 describe-instances --query "Reservations[*].Instances[*].{Instance:InstanceId,Name:Tags[?Key=='Name']|[0].Value}" --output table --color off
+aws ec2 describe-instances --query "Reservations[*].Instances[*].{InstanceID:InstanceId,Type:InstanceType,Name:Tags[?Key=='Name']|[0].Value,Status:State.Name}" --output table --color off
 
 echo -----------------------------------------
 
@@ -16,25 +16,75 @@ set /p id="Enter the InstanceID you want to manage: "
 echo -----------------------------------------
 
 :getConfirmation
-set /p ActionInstance=Need to [check/start/stop] "%id%" Instance? 
+set /p ActionInstance=Need to [check/start/stop/change] "%id%" Instance? 
 if /I "%ActionInstance%"=="start" goto :StartInstance
 if /I "%ActionInstance%"=="stop" goto :StopIstance
 if /I "%ActionInstance%"=="check" goto :CheckIstance
+if /I "%ActionInstance%"=="change" goto :GetIstance
 REM added goto getConfirmation in case of invalid responses
 goto getConfirmation
 
 :StartInstance
 aws ec2 start-instances --instance-ids %id%
+timeout 2
+aws ec2 describe-instances --instance-id %id% --query "Reservations[*].Instances[*].{Instance:InstanceId,PublicIP:PublicIpAddress,Name:Tags[?Key=='Name']|[0].Value,Status:State.Name}" --output table --color off
 timeout 5
 goto ExitProgram
 
 :StopIstance
 aws ec2 stop-instances --instance-ids %id%
+timeout 2
+aws ec2 describe-instances --instance-id %id% --query "Reservations[*].Instances[*].{Instance:InstanceId,PublicIP:PublicIpAddress,Name:Tags[?Key=='Name']|[0].Value,Status:State.Name}" --output table --color off
 timeout 5
 goto ExitProgram
 
 :CheckIstance
-aws ec2 describe-instances --instance-id %id% | find "Name"
+aws ec2 describe-instances --instance-id %id% --query "Reservations[*].Instances[*].{Instance:InstanceId,PublicIP:PublicIpAddress,Type:InstanceType,Name:Tags[?Key=='Name']|[0].Value,Status:State.Name}" --output table --color off
+timeout 5
+goto ExitProgram
+
+:GetIstance
+set /p ActionChangeInstance=Set instance "%id%" type to [t3.small/t3.medium/t3.large/t3.xlarge/t3.2xlarge]:
+if /I "%ActionChangeInstance%"=="t3.small" goto :t3small
+if /I "%ActionChangeInstance%"=="t3.medium" goto :t3medium
+if /I "%ActionChangeInstance%"=="t3.large" goto :t3large
+if /I "%ActionChangeInstance%"=="t3.xlarge" goto :t3xlarge
+if /I "%ActionChangeInstance%"=="t3.2xlarge" goto :t32xlarge
+REM added goto getConfirmation in case of invalid responses
+goto getConfirmation
+
+:t3small
+aws ec2 modify-instance-attribute --instance-id %id% --instance-type "{\"Value\": \"t3.small\"}"
+timeout 2
+aws ec2 describe-instances --instance-id %id% --query "Reservations[*].Instances[*].{Instance:InstanceId,Type:InstanceType,Name:Tags[?Key=='Name']|[0].Value}" --output table --color off
+timeout 5
+goto ExitProgram
+
+:t3medium
+aws ec2 modify-instance-attribute --instance-id %id% --instance-type "{\"Value\": \"t3.medium\"}"
+timeout 2
+aws ec2 describe-instances --instance-id %id% --query "Reservations[*].Instances[*].{Instance:InstanceId,Type:InstanceType,Name:Tags[?Key=='Name']|[0].Value}" --output table --color off
+timeout 5
+goto ExitProgram
+
+:t3large
+aws ec2 modify-instance-attribute --instance-id %id% --instance-type "{\"Value\": \"t3.large\"}"
+timeout 2
+aws ec2 describe-instances --instance-id %id% --query "Reservations[*].Instances[*].{Instance:InstanceId,Type:InstanceType,Name:Tags[?Key=='Name']|[0].Value}" --output table --color off
+timeout 5
+goto ExitProgram
+
+:t3xlarge
+aws ec2 modify-instance-attribute --instance-id %id% --instance-type "{\"Value\": \"t3.xlarge\"}"
+timeout 2
+aws ec2 describe-instances --instance-id %id% --query "Reservations[*].Instances[*].{Instance:InstanceId,Type:InstanceType,Name:Tags[?Key=='Name']|[0].Value}" --output table --color off
+timeout 5
+goto ExitProgram
+
+:t32xlarge
+aws ec2 modify-instance-attribute --instance-id %id% --instance-type "{\"Value\": \"t3.2xlarge\"}"
+timeout 2
+aws ec2 describe-instances --instance-id %id% --query "Reservations[*].Instances[*].{Instance:InstanceId,Type:InstanceType,Name:Tags[?Key=='Name']|[0].Value}" --output table --color off
 timeout 5
 goto ExitProgram
 
